970e864d6bb5adaa23def51d1d9a8d7aaa3643d7
Peiqin <daisygupq@gmail.com>
2018-04-09 18:23:13 -0700

    update union-find

--- Algorithms/UnionFind/DisjointSet.py
+++ Algorithms/UnionFind/DisjointSet.py
@@ -6,16 +6,14 @@ Supports two operations:
 It should be online algorithm.
 """
 
+import unittest
+
 
 class DisjointSetByArray(object):
-    def __init__(self):
-        self._disjoint_set = []
-
-    def create_set(self, x):
-        """
-            creates a new set with one element {x}.
-        """
-        self._disjoint_set[x] = x
+    def __init__(self, N):
+        self._id = list(range(N))
+        self._rank = [0] * N
+        self._count = N
 
     def union(self, x, y):
         """
@@ -23,25 +21,73 @@ class DisjointSetByArray(object):
             the set that contains element (x and y are in
             different sets). The original sets will be destroyed.
         """
-        if self._disjoint_set[x] < self._disjoint_set[y]:
-            self._disjoint_set[x] = self._disjoint_set[y]
+        id = self._id
+        rank = self._rank
+        i = self.find(x)
+        j = self.find(y)
+
+        if i == j:
+            return
+
+        self._count -= 1
+        # Weighted union
+        if self._rank[i] < self._rank[j]:
+            id[i] = j
+        elif self._rank[i] > self._rank[j]:
+            id[j] = i
         else:
-            self._disjoint_set[y] = self._disjoint_set[x]
+            id[j] = i
+            rank[i] += 1
 
     def find(self, x):
         """
              returns the representative or a pointer to the representative of the set that contains element x.
         """
-        if self._disjoint_set[x] < 0:
-            return x
-        else:
-            self._disjoint_set[x] = self.find(self._disjoint_set[x])
-            return self._disjoint_set[x]
+        id = self._id
+        while x != id[x]:
+            x = id[x] = id[id[x]]  # Path compression
+        return x
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def get_rank(self):
+        return self._rank
+
+
+class ListNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.repr = None
+
+    def set_repr(self, repr):
+        self.repr = repr
+
+    def set_next(self, next):
+        self.next = next
 
 
 class DisjointSetByTree(object):
+    def __init__(self, N):
+        pass
+
     def union(self, a, b):
         pass
 
-    def find(self, a, b):
+    def find(self, x):
         pass
+
+
+if __name__ == '__main__':
+    #unittest.main()
+    s = DisjointSetByArray(10)
+    print s.find(1)
+    print s.find(2)
+    print s.find(3)
+    s.union(1, 2)
+    s.union(1, 3)
+    print "after union"
+    print s.find(2)
+    print s.find(3)
+    print s.get_rank()
--- Algorithms/UnionFind/README.md
+++ Algorithms/UnionFind/README.md
@@ -1,7 +1,11 @@
 [**Disjoint Set Implementation**](DisjointSet.py)
-    
-    - **Array(数组实现)**: 用array来表示输入中每个元素的group，比如名为parent，其中parent[i]代表每个元素的group。这种实现union操作复杂度为O(n)，find操作复杂度为O(1)。
-    - **Tree(树实现)**：在同一棵树里的元素代表他们在同一个组（disjoint set）里，其中每棵树的根节点叫做the representative of the set。这种实现union操作复杂度为O(1)，find操作复杂度为O(n)。
+- **Definition(定义)**：a set of elements partitioned into a number of disjoint (non-overlapping) subsets.
+- **Array(数组实现)**: 用array来表示输入中每个元素的group，比如名为parent，其中parent[i]代表每个元素的group。这种实现union操作复杂度为O(n)，find操作复杂度为O(1)。
+- *Linked List(链表实现)*：
+- **Tree(树实现)**：在同一棵树里的元素代表他们在同一个组（disjoint set）里，其中每棵树的根节点叫做the representative of the set。这种实现union操作复杂度为O(1)，find操作复杂度为O(n)。
+- **优化**
+    - **Path Compression (路径压缩)**
+    - **Weighted Union (带权重的union)** 
 
 ## Questions
 - [Number of Islands](NumberOfIslands.py)
@@ -11,4 +15,6 @@
 
 
 ## References
+- Disjoint-set data structure: [https://en.wikipedia.org/wiki/Disjoint-set_data_structure](https://en.wikipedia.org/wiki/Disjoint-set_data_structure)
+- MIT Courseware: [https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-046j-design-and-analysis-of-algorithms-spring-2012/lecture-notes/MIT6_046JS12_lec16.pdf](https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-046j-design-and-analysis-of-algorithms-spring-2012/lecture-notes/MIT6_046JS12_lec16.pdf)
 - [https://www.topcoder.com/community/data-science/data-science-tutorials/disjoint-set-data-structures/](https://www.topcoder.com/community/data-science/data-science-tutorials/disjoint-set-data-structures/)
